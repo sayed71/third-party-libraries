cf32708c335414c5a135d1ac4444d08fa9297d70
Kazi Sayed Hossain <sayedmasud69@gmail.com>
2022-08-12 15:15:56 +0600

    Add files via upload

---
+++ Messanger HTML Design/js/scripts.js
@@ -0,0 +1,13 @@
+
+function newMessage() {
+	message = $(".message-input input").val();
+	if($.trim(message) == '') {
+		return false;
+	}
+	$('<li class="sent"><img src="http://emilcarlsson.se/assets/mikeross.png" alt="" /><p>' + message + '</p></li>').appendTo($('.messages ul'));
+	$('.message-input input').val(null);
+	$('.contact.active .preview').html('<span>You: </span>' + message);
+	$(".messages").animate({ scrollTop: $(document).height() }, "fast");
+};
+
+
